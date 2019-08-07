d70adbcac7b3475420fba366afc66825d9fafabc
Amit Arora <amit.arora@hughes.com>
2019-08-07 19:03:28 -0400

    pivot centroids

--- ml_kmeans.sql
+++ ml_kmeans.sql
@@ -13,3 +13,50 @@ OPTIONS
   FROM
     `bigquery-public-data.samples.natality`
   WHERE RAND() <= 0.001  
+
+WITH
+  T AS (
+  SELECT
+    centroid_id,
+    ARRAY_AGG(STRUCT(feature AS name,
+        ROUND(numerical_value,1) AS value)
+    ORDER BY
+      centroid_id) AS cluster
+  FROM
+    ML.CENTROIDS(MODEL `summer-project-1.my_first_dataset.kmeans_natality_model`)
+  GROUP BY
+    centroid_id )
+SELECT
+  CONCAT('Cluster#', CAST(centroid_id AS STRING)) AS centroid,
+  (
+  SELECT
+    value
+  FROM
+    UNNEST(cluster)
+  WHERE
+    name = 'weight_pounds') AS weight_pounds,
+  (
+  SELECT
+    value
+  FROM
+    UNNEST(cluster)
+  WHERE
+    name = 'is_male') AS is_male,
+  (
+  SELECT
+    value
+  FROM
+    UNNEST(cluster)
+  WHERE
+    name = 'gestation_weeks') AS gestation_weeks,
+  (
+  SELECT
+    value
+  FROM
+    UNNEST(cluster)
+  WHERE
+    name = 'mother_race') AS mother_race
+FROM
+  T
+ORDER BY
+  centroid_id ASC
